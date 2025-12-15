d5fb7910cdc4ccbdd6371a2e00e7314edf182833
Vancha Omer <vancha@paywithatoa.co.uk>
2025-12-15 09:26:13 +0530

    Initialize string calculator project with basic structure and dependencies

---
+++ lib/string_calculator.dart
@@ -0,0 +1,6 @@
+// TODO: Put public facing types in this file.
+
+/// Checks if you are awesome. Spoiler: you are.
+class StringCalculator {
+  // TODO: Implement String Calculator
+}
---
+++ test/string_calculator_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('String Calculator ...', () async {
+    // TODO: Implement test
+  });
+}
